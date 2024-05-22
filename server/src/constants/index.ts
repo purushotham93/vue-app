6e8495472e082b95a65f02b0cb0a6960740afe07
purushotham-puram <purushotham.puram@cantire.com>
2024-05-22 19:19:10 +0530

    setup axios and fetch it via vuex

---
+++ server/src/constants/index.ts
@@ -0,0 +1,5 @@
+const COVID_APIS = {
+    LATEST_STATS: "https://api.rootnet.in/covid19-in/stats/latest",
+  };
+  export { COVID_APIS };
+  
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,22 @@
-import express from 'express';
+// src/server.ts
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import { COVID_APIS } from "./constants/index";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/v1/covid-19/stats", async (req, res) => {
+  const result = await axios.get(COVID_APIS.LATEST_STATS);
+  console.log("data", result.data?.data);
+  res.send(result.data?.data);
 });
 
 app.listen(port, () => {
